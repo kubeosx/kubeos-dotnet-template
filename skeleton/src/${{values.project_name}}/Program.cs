608603a27d4364df8e74cde30d412ad32723581f
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-10-12 03:39:03 +0530

    update using

--- skeleton/src/${{values.project_name}}/Program.cs
+++ skeleton/src/${{values.project_name}}/Program.cs
@@ -1,4 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
